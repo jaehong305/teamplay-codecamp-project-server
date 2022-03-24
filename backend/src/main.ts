2fc640521dbb666e6b8c18e30ed2034aa2137fff
ljh305 <ljh3057713@gmail.com>
2022-03-24 15:39:52 +0900

    !HOTFIX: CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors({
-    origin: [process.env.CLIENT_URL],
+    origin: true, //[process.env.CLIENT_URL],
     credentials: true,
   });
   await app.listen(3000);
