4d8e39358b5fb748b4f5b66f76c54c1717466375
ljh305 <ljh3057713@gmail.com>
2022-03-25 13:42:22 +0900

    !HOTFIX: CORS

--- backend/src/apis/auth/auth.service.ts
+++ backend/src/apis/auth/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
       { secret: process.env.REFRESH_TOKEN_KEY, expiresIn: '2w' },
     );
     // res.setHeader('Set-Cookie', `refreshToken=${refreshToken}; path=/;`);
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); //process.env.CLIENT_URL);
+    res.setHeader('Access-Control-Allow-Origin', true); //process.env.CLIENT_URL);
     res.setHeader(
       'Set-Cookie',
       `refreshToken=${refreshToken}; path=/; domain=.ljh305.shop; SameSite=None; Secure; httpOnly;`,
